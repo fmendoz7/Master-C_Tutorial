2c6c22dc7c792e60b944c00eb43af91d83ef2168
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-02-17 13:53:27 -0700

    HelloWorld.c pushed

---
+++ HelloWorld.c
@@ -0,0 +1,9 @@
+//Standard input/output library 
+#include <stdio.h>
+
+main() 
+{
+    // \n represents newline character to create space
+    printf("Hello, World!\n");
+    printf("This is a very simple C program\n");
+}
